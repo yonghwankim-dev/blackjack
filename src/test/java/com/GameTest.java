7ca679277eaca5cf92b9099d86b79c9c884ce80f
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-12-12 14:54:07 +0900

    test: 플레이어 이름 입력 실패하는 테스트 추가
    적절하지 못한 이름(대문자로 3글자가 아닌 입력)이 주어졌을때를 테스트함

--- src/test/java/com/GameTest.java
+++ src/test/java/com/GameTest.java
@@ -1,15 +1,32 @@
 package com;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameTest {
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setupStreams(){
+        System.setOut(new PrintStream(output));
+    }
+
+    @AfterEach
+    public void restoreStreams(){
+        System.setOut(System.out);
+        output.reset();
+    }
+
     @DisplayName("플레이어의 이름을 물어보는 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"KYH"})
@@ -24,7 +41,25 @@ public class GameTest {
         assertThat(actual).isEqualTo(input);
     }
 
+    @DisplayName("플레이어의 이름을 적절하지 않은 이름으로 입력하는 경우")
+    @ParameterizedTest
+    @ValueSource(strings = {"KYHW\nKYH", "김용환\nKYH", "123\nKYH", " \nKYH", "abc\nKYH", "aBc\nKYH", "!@#\nKYH"})
+    public void testInputPlayerName_whenInputInvalidPlayerName_thenOutputWarning(String input){
+        //given
+        InputStream in = generateInputStream(input);
+        System.setIn(in);
+        Game game = new Game(null, null);
+        //when
+        String actual = game.inputPlayerName();
+        //then
+        assertThat(actual).isEqualTo("KYH");
+        String[] outputs = output.toString().split("\r\n");
+        assertThat(outputs[0]).isEqualTo("플레이어의 이름(3 english letters)은? ex) KYH");
+        assertThat(outputs[1]).isEqualTo("적절하지 않은 이름입니다. ex) KYH");
+    }
+
     private InputStream generateInputStream(String text){
         return new ByteArrayInputStream(text.getBytes());
     }
+
 }
