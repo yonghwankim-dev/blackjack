6f59f385b503ef96ae3c5d7d5d2d50aff21c5db6
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-12-12 13:48:04 +0900

    feat: 딜러 도메인 추가
    딜러는 카드패를 가지며 히트 또는 스탠드, 딜링을 수행합니다

---
+++ src/main/java/com/Dealer.java
@@ -0,0 +1,20 @@
+package com;
+
+import java.util.List;
+
+public class Dealer {
+    private List<Card> hands;
+
+    public void hit(){
+
+    }
+
+    public void stand(){
+
+    }
+
+    public void dealing(){
+
+    }
+
+}
