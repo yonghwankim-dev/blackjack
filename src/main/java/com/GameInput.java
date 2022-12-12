29083abedb9d366b8d153095ca469898979e2037
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-12-12 14:51:50 +0900

    feat: 플레이어의 이름 입력 검증 기능 추가
    GameInput 클래스는 GameInputValidate 클래스를 멤버로 추가하여 플레이어의 이름 입력에 대한 검증을 수행함.
    만약 이름 형식에 맞지 않는 입력이 들어오면 사용자에게 다시 입력하게함

--- src/main/java/com/GameInput.java
+++ src/main/java/com/GameInput.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import static com.GameInputValidate.validatePlayerName;
+
 public class GameInput {
     private BufferedReader br;
 
@@ -11,7 +13,18 @@ public class GameInput {
         this.br = new BufferedReader(new InputStreamReader(System.in));
     }
 
-    public String input() throws IOException {
-        return br.readLine();
+    public String inputPlayerName(){
+        String name = null;
+        System.out.println("플레이어의 이름(3 english letters)은? ex) KYH");
+        while(true){
+            try {
+                name = br.readLine();
+                if(!validatePlayerName(name)) throw new IOException();
+                break;
+            } catch (IOException e) {
+                System.out.println("적절하지 않은 이름입니다. ex) KYH");
+            }
+        }
+        return name;
     }
 }
