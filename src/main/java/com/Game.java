b317876d2338c7a4baf5107b87a1308ff2d9ec51
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-12-12 14:50:24 +0900

    refactor: 플레이어 이름 입력기능을 GameInput 클래스에게 위임
    게임 도메인이 플레이어의 이름을 묻는 기능을 GameInput 클래스에게 위임함

--- src/main/java/com/Game.java
+++ src/main/java/com/Game.java
@@ -1,11 +1,10 @@
 package com;
 
-import java.io.IOException;
-
 public class Game {
-    private Player player;
-    private Dealer dealer;
-    private GameInput gameInput;
+    private final Player player;
+    private final Dealer dealer;
+    private final GameInput gameInput;
+
     public Game(Player player, Dealer dealer) {
         this.player = player;
         this.dealer = dealer;
@@ -17,17 +16,7 @@ public class Game {
     }
 
     public String inputPlayerName(){
-        String name;
-        System.out.println("플레이어의 이름(3 english letters)은?");
-        while(true){
-            try {
-                name = gameInput.input();
-                break;
-            } catch (IOException e) {
-                System.out.println("적절하지 않은 이름입니다. ex) KYH");
-            }
-        }
-        return name;
+        return gameInput.inputPlayerName();
     }
 
     public void showHands(){
