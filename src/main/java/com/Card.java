ae26f41ef6877bbd497116eaf0e1dc81210a3af5
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-12-12 16:01:20 +0900

    feat: 카드 도메인 멤버에 final 추가
    카드 도메인의 멤버들에 final 키워드를 추가하여 수정하지 못하도록 함

--- src/main/java/com/Card.java
+++ src/main/java/com/Card.java
@@ -3,9 +3,9 @@ package com;
 import java.util.Objects;
 
 public class Card {
-    private String name;
-    private String shape;
-    private int value;
+    private final String name;
+    private final String shape;
+    private final int value;
 
     public Card(String name, String shape, int value) {
         this.name = name;
