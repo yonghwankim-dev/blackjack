23255af9faeda80adf264992fa82e14019356719
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-12-12 13:58:31 +0900

    feat: 게임 도메인 추가
    게임 도메인은 블랙잭 게임의 전체적인 진행을 수행함

---
+++ src/main/java/com/Game.java
@@ -0,0 +1,35 @@
+package com;
+
+public class Game {
+    private Player player;
+    private Dealer dealer;
+
+    public Game(Player player, Dealer dealer) {
+        this.player = player;
+        this.dealer = dealer;
+    }
+
+    public void start(){
+
+    }
+
+    public void inputPlayerName(){
+
+    }
+
+    public void showHands(){
+
+    }
+
+    public void inputHitOrStand(){
+
+    }
+
+    public void showDealerChose(){
+
+    }
+
+    public void showResult(){
+
+    }
+}
