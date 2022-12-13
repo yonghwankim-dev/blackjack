d0f585a87aa7a85ed858932f1ac5127bbdd0479d
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-12-13 18:32:43 +0900

    feat: 플레이어 도메인, 포인트 멤버 추가
    포인트 멤버는 플레이어가 게임 진행시 배팅할 수 있는 값

--- src/main/java/com/Player.java
+++ src/main/java/com/Player.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class Player {
     private final String name;
     private final List<Card> hands;
+    private int point;
 
     public Player(String name) {
         this.name = name;
         this.hands = new ArrayList<>();
+        this.point = 0;
     }
 
     public void hit(){
@@ -26,5 +28,13 @@ public class Player {
 
     public List<Card> getHands() {
         return hands;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void addPoint(int point){
+        this.point += point;
     }
 }
