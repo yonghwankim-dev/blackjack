ceed6b7919d3aaa0533b56f22eef3008dc3939b9
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-12-16 15:50:29 +0900

    refactor: 입력 기능 책임 맡김
    이름, 포인트, 배팅포인트 등의 입력기능을 기존 게임 도메인에서 플레이어 도메인으로 책임을 맡김

--- src/main/java/com/Player.java
+++ src/main/java/com/Player.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 public class Player extends User{
     private int point;
+    private PlayerInput playerInput;
+    public Player(){
+        this("");
+    }
 
     public Player(String name){
         this(name, 0);
@@ -12,14 +16,11 @@ public class Player extends User{
     public Player(String name, int point) {
         super(name, new ArrayList<>());
         this.point = point;
+        this.playerInput = new PlayerInput();
     }
 
-    public void hit(Game game){
-        game.dealingCardOneToUser(this);
-    }
-
-    public void stand(){
-
+    public void hit(){
+        // 구현해야함
     }
 
     public int getPoint() {
@@ -28,5 +29,25 @@ public class Player extends User{
 
     public void addPoint(int point){
         this.point += point;
+    }
+
+    public String inputName() {
+        return playerInput.inputName();
+    }
+
+    public Chose inputChose() {
+        return Chose.toChose(playerInput.inputChose(this));
+    }
+
+    public int inputInitialPoint() {
+        return playerInput.inputInitialPoint(getName());
+    }
+
+    public int inputBatting() {
+        return playerInput.inputBatting(this);
+    }
+
+    public String inputContinueGame(User dealer) {
+        return playerInput.inputContinueGame(dealer);
     }
 }
