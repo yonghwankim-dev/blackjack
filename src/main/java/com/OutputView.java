b2d092a99eab6f11288f8b7eae5fe0411acd2716
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-12-16 15:49:18 +0900

    refactor: 메서드명 변경 및 기능 위임받음
    메서드들을 정적(static)으로 만들고 메서드를 더 가독성있게 바꿈
    그리고 딜러의 기능을 위임받은 메서드를 추가함

--- src/main/java/com/OutputView.java
+++ src/main/java/com/OutputView.java
@@ -4,26 +4,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputView {
-    public void showPlayerInputConfirm(User player) {
+    public static void showUser(User user) {
         System.out.println("입력정보를 확인해주세요");
         System.out.println("-------------------------------");
-        System.out.printf("NAME : %-3s, POINT : %d%n", player.getName(), player.getPoint());
+        System.out.printf("NAME : %-3s, POINT : %d%n", user.getName(), user.getPoint());
         System.out.println("-------------------------------");
     }
 
-    public void showPlayerRemainingPoint(User player) {
-        System.out.printf("%3s님의 남은 포인트 : %d%n", player.getName(), player.getPoint());
+    public static void showRemainingPoint(User user) {
+        System.out.printf("%3s님의 남은 포인트 : %d%n", user.getName(), user.getPoint());
     }
 
-    public void showHandDealingToPlayer() {
-        System.out.printf("딜러 : 패를 나누어줬습니다.%n");
-    }
-
-    public void showPlayerChoseResult(User player, Chose chose) {
-        System.out.printf("딜러 : %3s님 %s를 선택하셨습니다.%n", player.getName(), chose.getName());
-    }
-
-    public void showHands(User player, User dealer) {
+    public static void showHands(User player, User dealer) {
         String dealerName = dealer.getName();
         String playerName = player.getName();
         int nameLen = Math.max(dealerName.length(), playerName.length());
@@ -36,29 +28,40 @@ public class OutputView {
         String dealerHandsContent = String.format(String.format("%%-%ds", handsLen), dealerHands);
         String playerHandsContent = String.format(String.format("%%-%ds", handsLen), playerHands);
 
-        System.out.printf("|  %-6s  |  %s  |  SCORE  |%n", nameHeader, handsHeader);
+        System.out.printf("|  %-6s  |  %s  |  %-5s  |%n", nameHeader, handsHeader, "SCORE");
         System.out.printf("|  %-6s  |  %s  |  %-5d  |%n", dealer.getName(), dealerHandsContent, dealer.getOpenedScore());
         System.out.printf("|  %-6s  |  %s  |  %-5d  |%n", player.getName(), playerHandsContent, player.getScore());
     }
 
-    private String formatHands(List<Card> cards){
+    private static String formatHands(List<Card> cards){
         return cards.stream().map(Card::toString).collect(Collectors.joining(","));
     }
 
-    public void showWinner(User user){
+    public static void showWinner(User user){
         System.out.printf("%s 승%n", user.getName());
     }
 
-    public void showDraw(){
+    public static void showDraw(){
         System.out.println("무승부");
     }
 
-    public void showStartGame() {
+    public static void showStartGame() {
         System.out.println("게임을 시작합니다.");
     }
 
-    public void showGameEnd(Player player) {
-        System.out.printf("%s님 포인트 : %d%n",player.getName(), player.getPoint());
+    public static void showPoint(User user){
+        System.out.printf("%s님 포인트 : %d%n", user.getName(), user.getPoint());
+    }
+
+    public static void showGameEnd() {
         System.out.println("게임을 종료합니다.");
+    }
+
+    public static void alertDealing(String name) {
+        System.out.printf("%s : 패를 나누어줬습니다.%n", name);
+    }
+
+    public static void alertChose(Dealer dealer, Player player, Chose chose) {
+        System.out.printf("%s : %3s님 %s를 선택하셨습니다.%n", dealer.getName(), player.getName(), chose.getName());
     }
 }
