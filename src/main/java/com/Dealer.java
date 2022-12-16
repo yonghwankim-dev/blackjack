bcfeb8ba2f7f2c1e50e561634a5ff230191faa31
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-12-16 15:44:34 +0900

    refactor: OutputView 클래스에게 위임 및 카드뽑기 기능 책임 맡게함
    alert로 시작하는 메서드들은 OutputView 클래스에게 위임함
    카드뽑기는 딜러가 하기 때문에 카드 리스트(cards)를
    멤버로 가지게 하고 메서드를 Game 도메인에서 이관함

--- src/main/java/com/Dealer.java
+++ src/main/java/com/Dealer.java
@@ -2,26 +2,39 @@ package com;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Dealer extends User{
+    private List<Card> cards;
+
     public Dealer() {
         super("DEALER", new ArrayList<>());
+        this.cards = createCardDeck(4);
     }
 
-    public void hit(Game game){
-        hit(game, CardStatus.OPEN);
+    public List<Card> createCardDeck(int n){
+        List<Card> result = new ArrayList<>();
+        Shape[] shapes = {Shape.HEART, Shape.DIAMOND, Shape.CLOVER, Shape.SPADE};
+        String[] names = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        int[] values = {11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
+
+        for(int i = 0; i < n; i++){
+            int shape_idx = i % shapes.length;
+            for(int j = 0; j < names.length; j++){
+                result.add(new Card(names[j], shapes[shape_idx], values[j]));
+            }
+        }
+
+        return result;
     }
 
-    public void hit(Game game, CardStatus status){
-        game.dealingCardOneToUser(this, status);
+    public void hit(){
+        hit(CardStatus.CLOSE);
     }
 
-    public void stand(){
-
-    }
-
-    public void dealing(){
-
+    public void hit(CardStatus status){
+        Card card = dealingCard(status);
+        getHands().add(card);
     }
 
     public boolean isNeedHit(){
@@ -34,23 +47,47 @@ public class Dealer extends User{
     }
 
     public void closeAllCardExceptOneCard() {
-        List<Card> hands = getHands();
         // 모든 카드 비공개 상태로 설정
-        hands.stream().forEach(c->c.toClose());
+        closeAllCard();
 
         // 첫번째 카드만 오픈 상태로 설정
-        hands.get(0).toOpen();
+        getHands().get(0).toOpen();
+    }
+
+    private void closeAllCard(){
+        getHands().forEach(Card::toClose);
     }
 
     public void openAllCard() {
-        getHands().stream().forEach(Card::toOpen);
+        getHands().forEach(Card::toOpen);
     }
 
     public void alertDealing() {
-        System.out.printf("%s : 패를 나누어줬습니다.%n", getName());
+        OutputView.alertDealing(getName());
     }
 
-    public void alertChose(User user, Chose chose) {
-        System.out.printf("%s : %3s님 %s를 선택하셨습니다.%n", getName(), user.getName(), chose.getName());
+    public void alertChose(Player player, Chose chose) {
+        OutputView.alertChose(this, player, chose);
+    }
+
+    public void dealingTwoCard(Player player) {
+        for(int i = 0; i < 2; i++){
+            player.addCard(dealingCard());
+            addCard(dealingCard());
+        }
+        closeAllCardExceptOneCard();
+        alertDealing();
+    }
+
+    private Card dealingCard() {
+        return dealingCard(CardStatus.OPEN);
+    }
+
+    private Card dealingCard(CardStatus status) {
+        Random random = new Random();
+        int idx = random.ints(0, cards.size()).findFirst().getAsInt();
+        Card card = cards.remove(idx);
+        card.setStatus(status);
+        return card;
     }
 }
